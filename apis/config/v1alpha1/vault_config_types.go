d92428d5218dff1312e8146bd00d712a592d4cb9
aaronschweig <aaron.schweig@gmail.com>
2023-05-11 12:04:27 +0200

    adds kuberntes auth to ess-plugin-vault
    Signed-off-by: aaronschweig <aaron.schweig@gmail.com>

--- apis/config/v1alpha1/vault_config_types.go
+++ apis/config/v1alpha1/vault_config_types.go
@@ -39,6 +39,9 @@ type VaultConfigSpec struct {
 	// Server is the url of the Vault server, e.g. "https://vault.acme.org"
 	Server string `json:"server"`
 
+	// Namesoace is the Namespace of vault on which to operate
+	Namespace string `json:"namespace,omitempty"`
+
 	// MountPath is the mount path of the KV secrets engine.
 	MountPath string `json:"mountPath"`
 
@@ -65,6 +68,10 @@ const (
 	// authenticate to Vault.
 	// https://www.vaultproject.io/docs/auth/token
 	VaultAuthToken VaultAuthMethod = "Token"
+	// VaultAuthKubernetes indicates that "Kubernetes Auth" will be used to
+	// authenticate to Vault.
+	// https://developer.hashicorp.com/vault/docs/auth/kubernetes
+	VaultAuthKubernetes VaultAuthMethod = "Kubernetes"
 )
 
 // VaultAuthTokenConfig represents configuration for Vault Token Auth Method.
@@ -79,6 +86,36 @@ type VaultAuthTokenConfig struct {
 	v1.CommonCredentialSelectors `json:",inline"`
 }
 
+// ServiceAccountTokenSourceConfig represents configuration to retrieve a
+// Service Account Token
+type ServiceAccountTokenSourceConfig struct {
+	// Source of the credentials.
+	// +kubebuilder:validation:Enum=None;Secret;Environment;Filesystem
+	Source v1.CredentialsSource `json:"source"`
+
+	// CommonCredentialSelectors provides common selectors for extracting
+	// credentials.
+	v1.CommonCredentialSelectors `json:",inline"`
+}
+
+// VaultAuthKubernetesConfig represents configuration for Vault Token Auth Method.
+// https://developer.hashicorp.com/vault/docs/auth/kubernetes
+type VaultAuthKubernetesConfig struct {
+	// Role should be the name of the role in Vault that was created with
+	// this app's Kubernetes service account bound to it
+	Role string `json:"role"`
+
+	// MountPath of the kubernetes secret engine in Vault
+	// +optional
+	MountPath string `json:"mountPath,omitempty"`
+
+	// ServiceAccountTokenSource allows to specify from where to retrieve the
+	// ServiceAccount Token in case it is not mounted under the default path
+	// `/var/run/secrets/kubernetes.io/serviceaccount/token`
+	// +optional
+	ServiceAccountTokenSource *ServiceAccountTokenSourceConfig `json:"serviceAccountTokenSource,omitempty"`
+}
+
 // VaultAuthConfig required to authenticate to a Vault API.
 type VaultAuthConfig struct {
 	// Method configures which auth method will be used.
@@ -86,6 +123,9 @@ type VaultAuthConfig struct {
 	// Token configures Token Auth for Vault.
 	// +optional
 	Token *VaultAuthTokenConfig `json:"token,omitempty"`
+	// Kubernetes configes Kubernetes Auth for Vault
+	// +optional
+	Kubernetes *VaultAuthKubernetesConfig `json:"kubernetes,omitempty"`
 }
 
 // VaultCABundleConfig represents configuration for configuring a CA bundle.
